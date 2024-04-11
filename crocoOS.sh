39776d9dfac12e8ca03c16d83d2cc8380c3e7c2a
Karol156 <149793440+karol157@users.noreply.github.com>
2024-04-11 15:36:53 +0200

    Update crocoOS.sh

--- crocoOS.sh
+++ crocoOS.sh
@@ -13,14 +13,14 @@ echo -ne "
   ╚═════╝ ╚═╝  ╚═╝  ╚══════╝   ╚═════╝  ╚══════╝
   ------------------------------------------------------------------------
   "
-    ( bash $SCRIPTS_DIR/startup.sh)|& tee startup.log
+    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
-    ( bash $SCRIPTS_DIR/preinstall.sh)|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/CrocoOS/scripts/setup.sh)|& tee 1-setup.log
-    if [[ ! $DESKTOM_ENV == server ]]; then
-        (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/CrocoOS/scripts/user.sh ) |& tee user.lot
+    ( bash $SCRIPT_DIR/scripts/preinstall.sh )|& tee 0-preinstall.log
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/setup.sh )|& tee 1-setup.log
+    if [[ ! $DESKTOP_ENV == server ]]; then
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/user.sh )|& tee 2-user.log
     fi
-    ( arch-chroot /mnt $HOME/CrocoOS/scripts/post-setup.sh )|& tee 3-post-setup.log
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/post-setup.sh )|& tee 3-post-setup.log
     cp -v *.log /mnt/home/$USERNAME
 
 echo -ne "
